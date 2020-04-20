1a717569c5d0bd7028cf3ea9670baa538f820175
yaramohamed98 <58967803+yaramohamed98@users.noreply.github.com>
2020-04-19 22:51:50 -0500

    Update movie_steps.rb

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,14 +2,16 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
+  
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -18,7 +20,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.should =~ /#{e1}.*#{e2}/m
+  
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,13 +29,14 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  unless uncheck.eql? 'un'
+    rating_list.split(',').each { |var| step %{I check "ratings_#{var}"} }
+  else
+    rating_list.split(',').each { |var| step %{I uncheck "ratings_#{var}"} }
+  end
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page.body.scan(/<tr>/).count).to eq(Movie.all.count + 1)
+ 
 end
